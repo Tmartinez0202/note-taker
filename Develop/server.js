12afab2d50a68210281693fbb5749dfe52362cdb
OkieKenobi <travismartinez88@gmail.comgit config --global user.name Tmartinez0202>
2020-07-28 22:32:58 -0600

    finished adding routes to server.js

---
+++ Develop/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+require("//routes/apiRoutes")(app);
+require("/routes/htmlRoutes")(app);
+
+
+
+app.listenerCount(PORT, () => console.log(`App listening 'http://localhost:${Port}'`));
